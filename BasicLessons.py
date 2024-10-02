37ff8c91857363c4bf91146a44b0aec7d0708909
Chris D. <crdumas@gmail.com>
2024-10-02 13:16:00 -0400

    Adding basic lessons.

---
+++ BasicLessons.py
@@ -0,0 +1,31 @@
+# Bob Smith
+# a fun fact, about me
+
+c1 = "CCCCC"
+c2 = "c    "
+c3 = "c    "
+c4 = "c    "
+c5 = "c    "
+c6 = "c    "
+c7 = "CCCCC"
+
+
+r1 = "rrrrr"
+r2 = "r   r"
+r3 = "r   r"
+r4 = "rrrrr"
+r5 = "r   r"
+r6 = "r   r"
+r7 = "r   r"
+
+space = "   "
+
+initials = c1 + space + r1 + "\n"
+initials += c2 + space + r2 + "\n"
+initials += c3 + space + r3 + "\n"
+initials += c4 + space + r4 + "\n"
+initials += c5 + space + r5 + "\n"
+initials += c6 + space + r6 + "\n"
+initials += c7 + space + r7 + "\n"
+
+print(initials)
