45667bb4455066dbd92d24cb7655920318efc6c0
Chris D. <crdumas@gmail.com>
2024-10-02 14:52:25 -0400

    Added ordering project.

--- BasicLessons.py
+++ BasicLessons.py
@@ -1,5 +1,6 @@
 # Bob Smith
 # a fun fact, about me
+from tkinter.tix import Balloon
 
 c1 = "CCCCC"
 c2 = "c    "
@@ -62,3 +63,38 @@ customer_one_total += customer_one_tax
 print("Customer One Items:"+ '\n')
 print(customer_one_itemization+ '\n')
 print("Customer One Total:" + str(round(customer_one_total, 2)))
+
+# 'Magic 8-Ball'
+
+import random
+
+
+name = 'Bob'
+question = 'some questions... '
+
+answer = ''
+
+random_number = random.randint(1, 8)
+
+if random_number == 1:
+    answer = 'Yes - definitely'
+elif random_number == 2:
+    answer = 'It is decidedly so'
+elif random_number == 3:
+    answer = 'Without a doubt'
+elif random_number == 4:
+    answer = 'Reply hazy, try again'
+elif random_number == 5:
+    answer = 'Ask again later'
+elif random_number == 6:
+    answer = 'Better not tell you now'
+elif random_number == 7:
+    answer = 'My sources say no'
+elif random_number == 8:
+    answer = 'Outlook not so good'
+else:
+    print('Whoops!')
+
+
+print(name + "asks " + question)
+print('Magic 8-Ball\'s answer: ' + answer + ' using random number:' + str(random_number))
