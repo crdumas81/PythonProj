0c2f42047c5cf521cf3a859dfd48d870ecd5b77f
Cdumas <crdumas81@gmail.com>
2024-10-03 22:26:16 -0400

    Added Pizza Lists

--- BasicLessons.py
+++ BasicLessons.py
@@ -158,3 +158,29 @@ print(gradebook)
 
 full_gradebook = gradebook+last_semester_gradebook
 print(full_gradebook)
+
+# pizza prices
+
+toppings = ["pepperoni","pineapple","cheese","sausage","olives","anchovies","mushrooms"]
+
+prices = [2,6,1,3,2,7,2]
+
+num_two_dollar_slices = prices.count(2)
+
+num_pizzas = len(toppings)
+
+print(f"We sell {num_pizzas} kinds of pizza!")
+
+pizza_and_prices = [[2, "pepperoni"],[6, "pineapple"],[1, "cheese"],[3, "sausage"],[2, "olives"],[7, "anchovies"],[2, "mushrooms"]]
+
+pizza_and_prices.sort()
+print(pizza_and_prices)
+
+cheapest_pizza = pizza_and_prices[0]
+
+priciest_pizza = pizza_and_prices.pop()
+
+pizza_and_prices.insert(4,[2.5,"peppers"])
+
+three_cheapest = pizza_and_prices[0:3]
+print(three_cheapest)
