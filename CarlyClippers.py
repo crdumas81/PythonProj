5cb4d5e1ae8b1e38ba721964afecb7c408a5eae8
Chris D. <crdumas@gmail.com>
2024-10-04 09:57:52 -0400

    Finished CarlyClippers.py

--- CarlyClippers.py
+++ CarlyClippers.py
@@ -3,3 +3,26 @@ hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk"
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for price in prices: total_price += price
+print(f"Total Price: {total_price}")
+
+average_price = total_price / len(prices)
+print(f"Average Haircut Price: {average_price}")
+
+new_prices = [p-5 for p in prices]
+
+total_revenue = 0
+for i in range(len(prices)): total_revenue += prices[i] * last_week[i]
+print(f"Total Revenue: {total_revenue}")
+
+average_daily_revenue = total_revenue / 7
+print(f"Average Daily Revenue: {average_daily_revenue}")
+
+
+
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)) if new_prices[i] < 30]
+
+print(cuts_under_30)
